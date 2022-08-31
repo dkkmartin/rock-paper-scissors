41b87e1aa42cb1d06d86786dc3e7a46464135f5e
Martin Bruun <martinbruun1995@gmail.com>
2022-08-31 19:59:16 +0200

    updated code

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const playerSelection = prompt('Pick: Rock, Paper or Scissors');
+console.log(game())
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) +1;
     
-    if (randomNumber === 1) {
-        return 'rock';
-    } else if (randomNumber === 2) {
-        return 'paper';
-    } else { 
-        return 'scissors';
+    switch (randomNumber) {
+        case 1:
+            return 'rock';
+        case 2:
+            return 'paper';
+        case 3:
+            return 'scissors';
     }
 }     
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection === 'rock' && computerSelection === 'paper') {
+        computerScore++;
         return 'You lose! Paper beats Rock';
+        
 
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++;
         return 'You win! Rock beats Scissors';
 
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++;
         return 'You win! Paper beats Rock';
         
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        computerScore++;
         return 'You Lose! Scissors beats Paper';
 
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++;
         return 'You win! Scissors beats Paper';
 
     } else if (playerSelection === 'scissors' && computerSelection === ' rock') {
+        computerScore++;
         return 'You lose! Rock beats scissors';
     } else {
         return 'It\'s a tie';
@@ -39,8 +52,7 @@ function game() {
     for (let i = 1; i < 6; i++) {
         console.log(`Game ${i} out of 5`);
         console.log(playRound(playerSelection, getComputerChoice()));
+        console.log(`Score: ${playerScore}-Player ${computerScore}-Computer`)
     }
 }
 
-const playerSelection = prompt('Pick: Rock, Paper or Scissors');
-console.log(game())
