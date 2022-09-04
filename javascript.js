2c42d0cc1f332ddd7d3f752701b4caadbf9e049f
Martin Bruun <martinbruun1995@gmail.com>
2022-09-04 11:06:56 +0200

    UI implemented

--- javascript.js
+++ javascript.js
@@ -1,35 +1,41 @@
 let playerScore = 0;
 let computerScore = 0;
+const result = document.querySelector('#result');
 
-const result = document.querySelector('#result')
-result.textContent = `${playerScore}-Human Computer-${computerScore}`;
 
 
 const btnRock = document.querySelector('#rock')
 btnRock.addEventListener('click', () => {
-    playRound('rock', getComputerChoice());
-    console.log(`${playerScore}-Player ${computerScore}-Computer`)
-    result.textContent = `${playerScore}-Human Computer-${computerScore}`;
+    roundOfFiveGames('rock', getComputerChoice());
 })
 
 const btnPaper = document.querySelector('#paper')
 btnPaper.addEventListener('click', () => {
-    playRound('paper', getComputerChoice());
-    console.log(`${playerScore}-Player ${computerScore}-Computer`)
-    result.textContent = `${playerScore}-Human Computer-${computerScore}`;
+    roundOfFiveGames('paper', getComputerChoice());
 })
 
 const btnScissors = document.querySelector('#scissors')
 btnScissors.addEventListener('click', () => {
-    playRound('scissors', getComputerChoice());
-    console.log(`${playerScore}-Player ${computerScore}-Computer`)
-    result.textContent = `${playerScore}-Human Computer-${computerScore}`;
+    roundOfFiveGames('scissors', getComputerChoice());
 })
 
+function roundOfFiveGames(playerSelection, computerSelection) {
+    playRound(playerSelection, computerSelection)
+    if (playerScore === 5) {
+        result.textContent = 'Human Wins!';
+        resetGame()
+    }
+    else if (computerScore === 5) {
+        result.textContent = 'Computer Wins!';
+        resetGame()
+    }
 
+}
 
-
-
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+}
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) +1;
@@ -45,41 +51,40 @@ function getComputerChoice() {
 }     
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
     if (playerSelection === computerSelection) {
-        console.log('TIE')
+        result.textContent = 'TIE-Human Computer-TIE';
     } 
     else if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
             // player lose
             computerScore++;
-            console.log(`${playerSelection}-VS-${computerSelection}`)
+            result.textContent = `${playerScore}-Human Computer-${computerScore}`;
         } else {
             // player win
             playerScore++;
-            console.log(`${playerSelection}-VS-${computerSelection}`)
+            result.textContent = `${playerScore}-Human Computer-${computerScore}`;
         }
     } 
     else if (playerSelection === 'paper') {
         if (computerSelection === 'scissors') {
             //player lose
             computerScore++;
-            console.log(`${playerSelection}-VS-${computerSelection}`)
+            result.textContent = `${playerScore}-Human Computer-${computerScore}`;
         } else {
             //player win
             playerScore++;
-            console.log(`${playerSelection}-VS-${computerSelection}`)
+            result.textContent = `${playerScore}-Human Computer-${computerScore}`;
         }
     } 
     else if (playerSelection === 'scissors') {
         if (computerSelection === 'rock') {
             // player lose
             computerScore++;
-            console.log(`${playerSelection}-VS-${computerSelection}`)
+            result.textContent = `${playerScore}-Human Computer-${computerScore}`;
         } else {
             // player win
             playerScore++;
-            console.log(`${playerSelection}-VS-${computerSelection}`)
+            result.textContent = `${playerScore}-Human Computer-${computerScore}`;
         }
     }
 }
